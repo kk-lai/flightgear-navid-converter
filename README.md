bb6559c9aacd771d439f053a2cc0964adbec8559
K.K. Lai <lai.kaikwong@gmail.com>
2022-04-09 18:37:14 +0800

    First version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # flightgear-navid-converter
 Convert earth_fix.dat and earth_nav.dat into Flightgear readable format
+
+To convert nav data, type the following command in cmd prompt (assuming your x-plane installed in D:\X-Plane 11)
+
+powershell -ExecutionPolicy Bypass -file .\xp2fg.ps1 nav "d:\X-Plane 11\Resources\default data\earth_nav.dat" nav.dat.gz
+
+To convert fixes, type the following command in cmd prompt (assuming your x-plane installed in D:\X-Plane 11)
+
+powershell -ExecutionPolicy Bypass -file .\xp2fg.ps1 fix "d:\X-Plane 11\Resources\default data\earth_fix.dat" fix.dat.gz
+
+Override the nav.data.gz and fix.dat.gz in Navaids folder of flight gear default data folder.
+
---
+++ xp2fg.ps1
@@ -0,0 +1,114 @@
+#
+# Wrote by Kevin King 9 Apr 2022
+#
+
+if ($args.Length -lt 3) {    
+    Write-Host "xp2fs.ps1 <nav/fix> <xplane ip file> <fg output file>"    
+    exit
+}
+
+$dtype=$args[0]
+$ifile=$args[1]
+$ofile=$args[2]
+
+#$dtype="fix"
+#$ifile="D:\X-Plane 11\Resources\default data\earth_fix.dat"
+#$ofile="D:\Temp\fix.dat"
+
+$stream_reader = New-Object System.IO.StreamReader($ifile)
+$dstFileStream = New-Object System.IO.FileStream($ofile,([IO.FileMode]::Create),([IO.FileAccess]::Write),([IO.FileShare]::None))
+$gzipStream = New-Object System.IO.Compression.GzipStream $dstFileStream, ([IO.Compression.CompressionMode]::Compress)
+$stream_writer = New-Object System.IO.StreamWriter($gzipStream)
+$lineNo = 1
+
+$dataCycle=Get-Date -Format "yyyy.MM"
+$build=Get-Date -Format "yyyyMMdd"
+
+if ($dtype -eq "nav") {
+    $meta="NAVXP810"
+    $version="810"
+} else {
+    $meta="FixXP700"
+    $version="600"
+}
+
+$stream_writer.WriteLine("I")
+$stream_writer.WriteLine("$version Version - data cycle $dataCycle, build $build, meta $meta. Data convert from x-plane")
+$stream_writer.WriteLine("")
+
+while (-not $stream_reader.EndOfStream)
+{
+    $iline=$stream_reader.ReadLine()
+    $lineNo++
+    if (($lineNo % 1000) -eq 0) {
+        Write-Host "$lineNo"    
+    }
+    $iline=$iline.Trim()
+    if ($iline.length -gt 0) {
+        $cols=-split($iline)
+        if ($cols[0].equals("I")) {
+            continue
+        }
+        if ($cols[0].equals("99")) {
+            break;
+        }
+        if ($dtype -eq "nav") {
+            $navType=[int]$cols[0]        
+            if ($navType -ge 2 -and $navType -le 13) {
+                $oline=""
+                switch($navType) {
+                    { 2,3,12,13 -contains $_ } {
+                        $pattern=0,1,2,3,4,5,6,7,10
+                    }
+                    { 4,5 -contains $_ } {
+                        # correct bearing
+                        $bearing=$cols[6] -as [double]
+                        $bearing=$bearing % 360.
+                        $cols[6]=$bearing.toString("#.###")
+                        $pattern=0,1,2,3,4,5,6,7,8,10,11
+                    }
+                    6 {
+                        $pattern=0,1,2,3,4,5,6,7,8,10,11
+                    }
+                    { 7,8,9 -contains $_ } {
+                        $pattern=0,1,2,3,4,5,6,7,8,10
+                    }
+                }
+                $oline=""
+                foreach($colNum in $pattern) {
+                    if ($oline.length -gt 0) {
+                        $oline=$oline+" "
+                    }
+                    $oline=$oline+ $cols[$colNum]
+                    if ($pattern.indexOf($colNum) -eq $pattern.Length-1) {
+                        for($j=$colNum+1;$j -lt $cols.Length;$j++) {
+                            $oline=$oline+" " + $cols[$j]
+                        }
+                    }
+                }
+                $stream_writer.WriteLine($oline)
+            }
+            if ($navType -eq 99) {
+                break
+            }
+        } else {
+            if ($cols[0] -eq "1101") {
+                continue;
+            }
+            $oline=""
+            for($j=0;$j -lt 3;$j++) {
+                if ($j -ne 0) {
+                    $oline=$oline+" "
+                }
+                $oline=$oline+$cols[$j]
+            }
+            $stream_writer.WriteLine($oline)
+        }
+
+    }
+}
+$stream_writer.WriteLine("99")
+$stream_writer.Close()
+$stream_reader.Close()
+
+
